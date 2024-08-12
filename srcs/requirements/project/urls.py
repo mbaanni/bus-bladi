5997d88a604c3aefde3da9c8e607930ca744f349
Mohamed Baanni <mbaanni@c1r3p5.1337.ma>
2024-08-12 18:09:09 +0100

    yml conf

--- srcs/requirements/project/urls.py
+++ srcs/requirements/project/urls.py
@@ -7,6 +7,3 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('manage_barcode.urls')),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
